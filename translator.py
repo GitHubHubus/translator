508e90e22c3a247939207d17014e39b70cb9acb3
Oleg Kochetkov <oleg.kochetkov@opensoftdev.ru>
2018-02-20 10:54:16 +0300

    refactor some code

--- translator.py
+++ translator.py
@@ -6,27 +6,27 @@ import requests
 import readline
 
 def getFormat(string, color = '\033[92m'):
- return color + string + '\033[0m'
+ sys.stdout.write(color + string + '\033[0m')
 
 def showSingleWordTranslate(data):
  value = data['terms'] if 'terms' in data else data['trans']
 
  if type(value) is unicode:
-  sys.stdout.write(getFormat('< ' + value))
+  getFormat('< ' + value)
  else:
   for i in range(0, len(value)):
-   sys.stdout.write(getFormat(' ' + str(i + 1) + ') ' + value[i] + '\n', '\033[94m'))
+   getFormat(' ' + str(i + 1) + ') ' + value[i] + '\n', '\033[94m')
 
 def showTextTranslate(data):
- sys.stdout.write(getFormat('< '))
+ getFormat('< ')
 
  for i in range(0, len(data)):
-  sys.stdout.write(getFormat(data[i]['trans']))
+  getFormat(data[i]['trans'])
 
 def translate(url):
  data = raw_input('> ')
 
- if data == '\exit':
+ if data == '\quit' or data == '\q':
   return False;
 
  output = requests.get(url + data).json()
@@ -41,16 +41,16 @@ def translate(url):
  return True
 
 def getHelp():
- print('-i - Interactive mode. Exit from this mode by enter in command line "\exit"')
- print('-r - Revert input/output language (without flag en -> ru and with ru -> en)')
+ str = ('-i - Interactive mode. Exit from this mode by enter in command line "\exit" \n' + 
+        '-r - Revert input/output language (without flag en -> ru and with ru -> en) \n')
+ sys.stdout.write(str)
 
 def getUrl(lg):
  return 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&dt=t&dt=bd&dj=1&tl=' + lg + '&ie=UTF-8&oe=UTF-8&text='
 
 def main():
  input = sys.argv
- language = 'en' if '-r' in input else 'ru'
- url = getUrl(language)
+ url = getUrl('en' if '-r' in input else 'ru')
 
  if '-i' in input:
   while True:
